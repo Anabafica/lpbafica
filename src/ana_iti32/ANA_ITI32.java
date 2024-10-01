d0e2ad5ae35670089ee77c73cf9fa6edf7db37d4
Anabafica <baficasilvaa@gmail.com>
2024-10-01 10:32:34 -0300

    versao inicial

---
+++ src/ana_iti32/ANA_ITI32.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ana_iti32;
+
+/**
+ *
+ * @author Aluno
+ */
+public class ANA_ITI32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
